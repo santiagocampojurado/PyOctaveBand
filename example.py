15586ecb40631cd754f6f638b18c362ea71dc789
Santiago Campo <id00706137@usal.es>
2025-03-04 17:00:13 +0100

    Refactor audio file processing and add error handling

--- example.py
+++ example.py
@@ -5,6 +5,8 @@ import os
 import logging
 import argparse
 from tqdm import tqdm
+import datetime
+
 
 
 logging.basicConfig(
@@ -15,32 +17,26 @@ logging.basicConfig(
 )
 
 
+
 def find_audio_folders(base_path):
-    """Recursively find all subdirectories containing an 'AUDIOMOTH' folder."""
     for root, dirs, files in os.walk(base_path):
         if 'AUDIO' in dirs:
             yield root
 
 
+
 def get_audiofiles(path):
-    """
-    Args:
-        path (str): The path to the directory containing the audio files.
-    Returns:
-        list: A list containing the full paths to all '.wav' files in the specified directory.
-    """    
     audio_files = [file for file in os.listdir(path) if file.lower().endswith('.wav')]
     return audio_files
+
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Calculate SPL levels for audio files in a directory')
     parser.add_argument('-p', '--path', type=str, required=True, help='Directory to be processed')
-    # parser max and min
     parser.add_argument('-lb', '--lower_bound', type=float, default=90.0, help='Lower bound for lineal differenciation')
     parser.add_argument('-ub', '--upper_bound', type=float, default=112.0, help='Upper bound for lineal differenciation')
-    # calibration constant from the multifunction
-    parser.add_argument('-t', '--threshold', type=int, default=94.0, help='Threshold constant for the microphone')
+    parser.add_argument('-t', '--threshold', type=int, default=94, help='Threshold constant for the microphone')
     return parser.parse_args()
 
 
@@ -58,9 +54,19 @@ def main():
     logging.info(f"Threshold multifuntion: {threshold_multifinction}")
     logging.info("")
 
+
+
+    # ----------------
+    # FIND AUDIO FILES
+    # ----------------
     audio_folders = list(find_audio_folders(base_path))
     logging.info(f"Found {len(audio_folders)} folders to process.")
 
+
+
+    # ----------------
+    # PROCESS FOLDERS & AUDIO FILES
+    # ----------------
     for subfolder in tqdm(audio_folders, desc='Processing folders'):
         logging.info(f"Processing audio files in: {subfolder}...")
         audio_path = os.path.join(subfolder, "AUDIO")
@@ -73,47 +79,69 @@ def main():
             logging.warning(f"No audio files found in: {audio_path}")
             continue
 
-
         for audio_file in tqdm(audio_files, desc='Processing audio files'):
-            # fs, data = wavfile.read(r"\\192.168.205.123\aac_server\CALIBRATION\AAC_SENSOR\3-Medidas\0001\AUDIO\20250303_000001.wav")
-            fs, data = wavfile.read(os.path.join(audio_path, audio_file))
-            print(f"Sample rate: {fs} Hz")
-            duration = len(data) / fs
-            print(f"Duration: {duration} seconds")
-            
+            try:
+                logging.info(f"Processing audio file: {audio_file}")
+                # Metadata
+                full_audio_path = os.path.join(audio_path, audio_file)
+                fs, data = wavfile.read(full_audio_path)
+                logging.info(f"Sample rate: {fs} Hz")
+                duration = len(data) / fs
+                logging.info(f"Duration: {duration} seconds")
+                window_size = fs  # 1-second window
+
+                # NAME SPLIT and TIMESTAMP
+                name_split = audio_file.split(".")[0]
+                start_timestamp = datetime.datetime.strptime(name_split, '%Y%m%d_%H%M%S')
+                timestamps = [start_timestamp + datetime.timedelta(seconds=sec) for sec in range(0, len(data) // fs)]
+
+                # ---------------
+                # MONO CONVERSION
+                # ---------------
+                if data.ndim > 1:
+                    data = data[:, 0]
+
+                # ----------------
+                # 1 SEC PROCESSING
+                # ----------------
+                results = [] 
+                freq_labels = None
+                for start_idx in range(0, len(data) - window_size + 1, window_size):
+                    segment = data[start_idx:start_idx + window_size]
+
+                    #octave band levels for the segment
+                    levels, freqs = PyOctaveBand.octavefilter(segment, fs, fraction=3)
+                    levels = [round(level, 2) for level in levels]
+                    
+                    if freq_labels is None:
+                        freq_labels = [f"{round(freq, 1)}Hz" for freq in freqs]
+                    results.append(levels)
+
+                # ----------------
+                # SAVE CSV FILE
+                # ----------------
+                df = pd.DataFrame(results, columns=freq_labels)
+                df.insert(0, "date", timestamps)
+                df.insert(0, "filename", audio_file)
+                
+                output_folder = audio_path.replace('3-Medidas', '5-Results')
+                if not os.path.exists(output_folder):
+                    os.makedirs(output_folder)
+                    logging.info(f"Created output folder: {output_folder}")
+
+                output_filename = f'leq_oct_{name_split}.csv'
+                output_path = os.path.join(output_folder, output_filename)
+                
+
+                #save
+                df.to_csv(output_path, index=False)
+                logging.info(f'Output saved to {output_path}')
+
+            except Exception as e:
+                logging.error(f"Error processing audio file: {audio_file}")
+                logging.error(e)
 
 
-            # mono
-            if data.ndim > 1:
-                data = data[:, 0]
-
-
-
-            n_seconds = len(data) // fs
-            results = [] 
-            freq_labels = None
-
-
-
-            for sec in range(n_seconds):
-                segment = data[sec * fs : (sec + 1) * fs]
-
-                # calculate octave band levels
-                levels, freqs = PyOctaveBand.octavefilter(segment, fs, fraction=3)
-                levels = [round(level, 2) for level in levels]
-                if freq_labels is None:
-                    freq_labels = [f"{round(freq, 1)}Hz" for freq in freqs]
-                results.append(levels)
-
-            
-
-            # create csv
-            df = pd.DataFrame(results, columns=freq_labels)
-            df.insert(0, "Second", range(1, n_seconds + 1))
-
-            # save csv final file
-            df.to_csv("frequency_levels_per_second.csv", index=False)
-            print("CSV file 'frequency_levels_per_second.csv' has been saved.")
 
 
 if __name__ == '__main__':
