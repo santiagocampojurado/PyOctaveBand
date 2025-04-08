c51de2f97e3b2c3981ef352d2fb5b32faba48a16
Santiago Campo <id00706137@usal.es>
2025-04-08 12:16:10 +0200

    Remove unused import and clean up spectrum column definitions in main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import argparse
 from utils import *
 from config import *
 
-import PyOctaveBand
 import PyOctaveBand_reduced
 
 
@@ -24,14 +23,6 @@ logging.basicConfig(
     filemode='w'
 )
 
-
-SPECTRUM_COLUMNS = ['31.6Hz', '63.1Hz', '125.9Hz', '251.2Hz', '501.2Hz',
-                    '1000.0Hz', '1995.3Hz', '3981.1Hz', '7943.3Hz', '12589.3Hz',
-                    '15848.9Hz'
-                ]
-
-SPECTRUM_COLUMNS_LITTLE = ['31.6Hz', '63.1Hz', '125.9Hz', '251.2Hz', '501.2Hz',
-                            '1000.0Hz', '1995.3Hz', '3981.1Hz']
 
 
 
